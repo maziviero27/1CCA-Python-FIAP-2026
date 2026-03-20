c3f5b2b08df2c7315a927e91be79ab86410be2f5
maziviero27 <arthur_mfaria@hotmail.com>
2026-03-20 11:31:41 -0300

    aula 02 variaveis e operadores

---
+++ Aula02-variaveis-operadores/Desafio2.py
@@ -0,0 +1,2 @@
+dia = input("digite o dia em que você nasceu: ")
+mes = input("Digite o mês que você nasceu: ")
---
+++ Aula02-variaveis-operadores/aula02a.py
@@ -0,0 +1,34 @@
+print('hello world')
+
+print(7+4)
+
+print('7'+ '4') #CONCATENAÇÃO DE STRINGS
+
+#COMENTARIO DE UMA LINHA
+'''
+COMENTARIOS 
+DE 
+MULTIPLAS 
+LINHAS
+'''
+nome = "Arthur"
+idade = 18 #int
+peso = 94.5 #float
+
+
+print(nome)
+print("idade: ", idade)
+print("peso: ", peso)
+
+print(f"Oiii {nome}!!!") #sintaxe melhor
+
+# INPUT - SIMULAÇÃO DE FORMS NO CMD
+nome = input("digite o seu nome: ")
+idade = int(input("digite sua idade: "))
+
+print(nome, idade)
+print(idade + 1)
+
+
+
+
