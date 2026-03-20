c7938d8979c55e21a25659b7dfab90c04fa21ddc
maziviero27 <arthur_mfaria@hotmail.com>
2026-03-20 11:35:15 -0300

    aula 02 variaveis e operadores

--- Aula02-variaveis-operadores/Desafio2.py
+++ Aula02-variaveis-operadores/Desafio2.py
@@ -1,2 +1,4 @@
 dia = input("digite o dia em que você nasceu: ")
 mes = input("Digite o mês que você nasceu: ")
+ano = input("Digite o ano que você nasceu: ")
+print(f"você nasceu em {dia} / {mes} / {ano}")
